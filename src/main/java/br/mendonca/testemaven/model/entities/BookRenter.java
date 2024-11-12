e057ff2832b9e4c548268f9901b1cbc05ae566ae
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2024-11-12 14:30:25 -0300

    feat: adicionar entidade BookRenter com atributos básicos e getters/setters

---
+++ src/main/java/br/mendonca/testemaven/model/entities/BookRenter.java
@@ -0,0 +1,47 @@
+package br.mendonca.testemaven.model.entities;
+
+public class BookRenter {
+    private String id;
+    private String name;
+    private String email;
+    private String address;
+    private String phoneNumber;
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        this.address = address;
+        return "";
+    }
+}
